dfbca5ff3ff130b4b50e05c95fb8ef8901333a1f
MrJeyhun <CeyhunzadeC@kapitalho.az>
2020-11-26 15:44:13 +0400

    adding event logic was written

--- src/js/eventsData.js
+++ src/js/eventsData.js
@@ -1,10 +1,20 @@
-let eventsData = [
+export let eventsData = [
     {
         id: 'post1',
         date: 'Mon, APR 09, 7:00 PM',
         description: 'This Thing Called Life: A Celebration of Prince',
         location: 'Mezzaine, San Francisco, CA',
         price: '30',
-        hashtags: '#music #party'
+        cover: 'https://s9.postimg.cc/y0sfm95gv/prince_f.jpg',
+        hashtags: '#Music #Party'
+    },
+    {
+        id: 'post2',
+        date: 'Tue, APR 10, 7:00 PM',
+        description: 'Best concert ever! Awesome night!',
+        location: 'Flarmonia Street, CA',
+        price: '25',
+        cover: 'https://s9.postimg.cc/y0sfm95gv/prince_f.jpg',
+        hashtags: '#Music #Party'
     }
 ]
