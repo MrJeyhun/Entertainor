b6ec2d827264d2700e21abbe4a6bf63f7cf6698d
MrJeyhun <CeyhunzadeC@kapitalho.az>
2020-11-30 19:15:27 +0400

    Create event modal was styled

--- src/js/renderEvents.js
+++ src/js/renderEvents.js
@@ -1,89 +1,93 @@
-import {eventsData} from './eventsData.js';
-import {openSlideModal} from './slideModal.js';
+import { eventsData } from './eventsData.js';
+import { openSlideModal } from './slideModal.js';
 
-const countDownModalOverlay = document.querySelector('.eventspage__countdown-modal-overlay');
+const countDownModalOverlay = document.querySelector('.countdown-modal-overlay');
 const eventsContainer = document.querySelector('.eventspage__events');
 
 export const renderEvents = () => {
-    eventsData.map((event) => {
-      console.log('rendering...')
-      //ah react, react...
-      const eventCard = document.createElement('div');
-      eventCard.className = 'eventspage__events__event-card';
+    eventsData.map(event => {
+        console.log('rendering...');
+        //ah react, react...
+        const eventCard = document.createElement('div');
+        eventCard.className = 'eventspage__events__event-card';
 
-      const eventCardCover = document.createElement('div');
-      eventCardCover.className = 'eventspage__events__event-card__cover';
-      const eventCardCover_image = document.createElement('div');
-      eventCardCover_image.className = 'eventspage__events__event-card__cover__image';
-      eventCardCover_image.style.backgroundImage = `url(${event.cover})`;
+        const eventCardCover = document.createElement('div');
+        eventCardCover.className = 'eventspage__events__event-card__cover';
+        const eventCardCover_image = document.createElement('div');
+        eventCardCover_image.className = 'eventspage__events__event-card__cover__image';
+        eventCardCover_image.style.backgroundImage = `url(${event.cover})`;
 
-      const eventCardInfo = document.createElement('div');
-      eventCardInfo.className = 'eventspage__events__event-card__info-section';
+        const eventCardInfo = document.createElement('div');
+        eventCardInfo.className = 'eventspage__events__event-card__info-section';
 
-      const eventCardInfo_dateContainer = document.createElement('div');
-      eventCardInfo_dateContainer.className = 'eventspage__events__event-card__info-section__date';
+        const eventCardInfo_dateContainer = document.createElement('div');
+        eventCardInfo_dateContainer.className = 'eventspage__events__event-card__info-section__date';
 
-      const eventCardInfo_dateContainer_date = document.createElement('span');
-      eventCardInfo_dateContainer_date.textContent = `${event.date.slice(0, event.date.length-3)}`
+        const eventCardInfo_dateContainer_date = document.createElement('span');
+        eventCardInfo_dateContainer_date.textContent = `${event.date.slice(0, event.date.length - 3)}`;
 
-      const eventCardInfo_description = document.createElement('div');
-      eventCardInfo_description.className = 'eventspage__events__event-card__info-section__description';
-      eventCardInfo_description.textContent = `${event.description}`;
+        const eventCardInfo_description = document.createElement('div');
+        eventCardInfo_description.className = 'eventspage__events__event-card__info-section__description';
+        eventCardInfo_description.textContent = `${event.description}`;
 
-      const eventCardInfo_loc_and_price = document.createElement('div');
-      eventCardInfo_loc_and_price.className = 'eventspage__events__event-card__info-section__loc-and-price card-bottom';
-      
-      const eventCardInfo_loc = document.createElement('span');
-      eventCardInfo_loc.className = 'eventspage__events__event-card__info-section__loc-and-price__loc';
-      eventCardInfo_loc.textContent = `${event.location}`;
+        const eventCardInfo_loc_and_price = document.createElement('div');
+        eventCardInfo_loc_and_price.className =
+            'eventspage__events__event-card__info-section__loc-and-price card-bottom';
 
-      const eventCardInfo_price = document.createElement('span');
-      eventCardInfo_price.className = 'eventspage__events__event-card__info-section__loc-and-price__price';
-      eventCardInfo_price.textContent = `$${event.price}`;
+        const eventCardInfo_loc = document.createElement('span');
+        eventCardInfo_loc.className = 'eventspage__events__event-card__info-section__loc-and-price__loc';
+        eventCardInfo_loc.textContent = `${event.location}`;
 
-      const eventCardInfo_hoversec = document.createElement('div');
-      eventCardInfo_hoversec.className = 'eventspage__events__event-card__info-section__hover-section card-bottom';
+        const eventCardInfo_price = document.createElement('span');
+        eventCardInfo_price.className = 'eventspage__events__event-card__info-section__loc-and-price__price';
+        eventCardInfo_price.textContent = `$${event.price}`;
 
-      const eventCardInfo_hoversec_hashtags = document.createElement('span');
-      eventCardInfo_hoversec_hashtags.className = 'eventspage__events__event-card__info-section__hover-section__hashtags';
-      eventCardInfo_hoversec_hashtags.textContent = `${event.hashtags}`;
+        const eventCardInfo_hoversec = document.createElement('div');
+        eventCardInfo_hoversec.className = 'eventspage__events__event-card__info-section__hover-section card-bottom';
 
-      const eventCardInfo_hoversec_getTicket = document.createElement('button');
-      eventCardInfo_hoversec_getTicket.className = 'eventspage__events__event-card__info-section__hover-section__get-ticket';
-      eventCardInfo_hoversec_getTicket.id = `${event.id}`;
-      eventCardInfo_hoversec_getTicket.textContent = 'Get ticket!';
+        const eventCardInfo_hoversec_hashtags = document.createElement('span');
+        eventCardInfo_hoversec_hashtags.className =
+            'eventspage__events__event-card__info-section__hover-section__hashtags';
+        eventCardInfo_hoversec_hashtags.textContent = `${event.hashtags}`;
 
-      //composing of elements
-      eventsContainer.appendChild(eventCard);
+        const eventCardInfo_hoversec_getTicket = document.createElement('button');
+        eventCardInfo_hoversec_getTicket.className =
+            'eventspage__events__event-card__info-section__hover-section__get-ticket';
+        eventCardInfo_hoversec_getTicket.id = `${event.id}`;
+        eventCardInfo_hoversec_getTicket.textContent = 'Get ticket!';
 
-      eventCard.appendChild(eventCardCover);
-      eventCard.appendChild(eventCardInfo);
+        //composing of elements
+        eventsContainer.appendChild(eventCard);
 
-      eventCardCover.appendChild(eventCardCover_image);
+        eventCard.appendChild(eventCardCover);
+        eventCard.appendChild(eventCardInfo);
 
-      eventCardInfo.appendChild(eventCardInfo_dateContainer);
-      eventCardInfo.appendChild(eventCardInfo_description);
-      eventCardInfo.appendChild(eventCardInfo_loc_and_price);
-      eventCardInfo.appendChild(eventCardInfo_loc_and_price);
-      eventCardInfo.appendChild(eventCardInfo_hoversec);
+        eventCardCover.appendChild(eventCardCover_image);
 
-      eventCardInfo_dateContainer.appendChild(eventCardInfo_dateContainer_date);
+        eventCardInfo.appendChild(eventCardInfo_dateContainer);
+        eventCardInfo.appendChild(eventCardInfo_description);
+        eventCardInfo.appendChild(eventCardInfo_loc_and_price);
+        eventCardInfo.appendChild(eventCardInfo_loc_and_price);
+        eventCardInfo.appendChild(eventCardInfo_hoversec);
 
-      eventCardInfo_loc_and_price.appendChild(eventCardInfo_loc);
-      eventCardInfo_loc_and_price.appendChild(eventCardInfo_price);
+        eventCardInfo_dateContainer.appendChild(eventCardInfo_dateContainer_date);
 
-      eventCardInfo_hoversec.appendChild(eventCardInfo_hoversec_hashtags);
-      eventCardInfo_hoversec.appendChild(eventCardInfo_hoversec_getTicket);
-    })
+        eventCardInfo_loc_and_price.appendChild(eventCardInfo_loc);
+        eventCardInfo_loc_and_price.appendChild(eventCardInfo_price);
 
-    const btnsOfEventCards = document.querySelectorAll('.eventspage__events__event-card__info-section__hover-section__get-ticket');
+        eventCardInfo_hoversec.appendChild(eventCardInfo_hoversec_hashtags);
+        eventCardInfo_hoversec.appendChild(eventCardInfo_hoversec_getTicket);
+    });
 
-    if (btnsOfEventCards.length != 0 ) {
+    const btnsOfEventCards = document.querySelectorAll(
+        '.eventspage__events__event-card__info-section__hover-section__get-ticket',
+    );
+
+    if (btnsOfEventCards.length != 0) {
         for (const btn of btnsOfEventCards) {
-            btn.addEventListener("click", (event) => {
+            btn.addEventListener('click', event => {
                 openSlideModal(countDownModalOverlay, event.target.id);
-            })
+            });
         }
-    }  
-}
-
+    }
+};
--- src/js/slideModal.js
+++ src/js/slideModal.js
@@ -1,29 +1,29 @@
-import {eventsData} from './eventsData.js';
-const modals = document.querySelectorAll('.eventspage__modal');
-const countDownModalOverlay = document.querySelector('.eventspage__countdown-modal-overlay');
-const addEventModalOverlay = document.querySelector('.eventspage__add-event-modal-overlay');
-const closeSlideModalBtns = document.querySelectorAll('.eventspage__modal__close');
+import { eventsData } from './eventsData.js';
+const modals = document.querySelectorAll('.modal');
+const countDownModalOverlay = document.querySelector('.countdown-modal-overlay');
+const addEventModalOverlay = document.querySelector('.add-event-modal-overlay');
+const closeSlideModalBtns = document.querySelectorAll('.modal__close');
 
 let interval;
 export const openSlideModal = (selectedModal, eventId = null) => {
-    eventsData.map((event) => {
+    eventsData.map(event => {
         if (eventId == event.id) {
             interval = setInterval(() => {
                 setCountDown(event.date);
-            }, 1000);   
+            }, 1000);
         }
-    })
+    });
 
     setTimeout(() => {
         for (const modal of modals) {
             modal.style.animation = 'modal-open .8s ease';
         }
         selectedModal.style.display = 'flex';
-    }, 500)
-}
+    }, 500);
+};
 
 const closeSlideModal = () => {
-    console.log('closing...')
+    console.log('closing...');
     for (const modal of modals) {
         modal.style.animation = 'modal-close .7s ease';
     }
@@ -32,34 +32,32 @@ const closeSlideModal = () => {
     setTimeout(() => {
         countDownModalOverlay.style.display = 'none';
         addEventModalOverlay.style.display = 'none';
-    }, 600);    
-}
+    }, 600);
+};
 
 for (const closeSlideModalBtn of closeSlideModalBtns) {
     closeSlideModalBtn.addEventListener('click', closeSlideModal);
 }
 
-
 //countdown logic
-const setCountDown = (eventDate) => {
+const setCountDown = eventDate => {
+    const countDate = new Date(eventDate).getTime();
 
-  const countDate = new Date(eventDate).getTime();
+    const now = new Date().getTime();
+    let gap = countDate - now;
 
-  const now = new Date().getTime();
-  let gap = countDate - now;
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    let d = Math.floor(gap / day);
+    let h = Math.floor((gap % day) / hour);
+    let m = Math.floor((gap % hour) / minute);
+    let s = Math.floor((gap % minute) / second);
 
-  let d = Math.floor(gap / day);
-  let h = Math.floor((gap % day) / hour);
-  let m = Math.floor((gap % hour) / minute);
-  let s = Math.floor((gap % minute) / second);
-
-  document.getElementById("day").innerText = d;
-  document.getElementById("hour").innerText = h;
-  document.getElementById("minute").innerText = m;
-  document.getElementById("second").innerText = s;
-}
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+};
